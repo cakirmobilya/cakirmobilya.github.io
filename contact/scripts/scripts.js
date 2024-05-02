5cfce21681922baf467da56e70fde8406bcda5ad
cakirmobilya <168763937+cakirmobilya@users.noreply.github.com>
2024-05-02 19:42:37 +0300

    Add files via upload

---
+++ contact/scripts/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    // Sayfa yüklendiğinde loaderı kapat
+    setTimeout(function() {
+        document.querySelector('.loader-container').style.display = 'none';
+    }, 500); // 7 saniye sonra loaderı kapat
+});
---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    // Sayfa yüklendiğinde loaderı kapat
+    setTimeout(function() {
+        document.querySelector('.loader-container').style.display = 'none';
+    }, 500); // 7 saniye sonra loaderı kapat
+});
+
+
+
+
